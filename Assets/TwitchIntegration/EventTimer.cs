2e195204fb6d04dbee66985ffcabade7da4aa415
Andrei Brasso <andrei.brasso@gmail.com>
2019-01-26 12:55:02 +0200

    Init + Twitch Integration

---
+++ Assets/TwitchIntegration/EventTimer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TwitchIRC))]
+[RequireComponent(typeof(TwitchResponses))]
+public class EventTimer : MonoBehaviour {
+
+    // Use this for initialization
+
+    private float time;
+    private bool timeIsTicking = false;
+    public float eventTime = 30.0f;
+
+    public Dictionary<string, List<string>> votes;
+
+    private TwitchResponses TR;
+    private TwitchIRC IRC;
+
+    void StartNewEvent()
+    {
+        IRC.SendMsg("A new event is starting soon, vote now with !vote and number of your option");
+
+        time = eventTime;
+        timeIsTicking = true;
+        
+        TR.InitVotes();
+        TR.openEvent = true;
+        
+        Debug.Log("StartEvent");
+    }
+
+    void EventStop()
+    {
+        timeIsTicking = false;
+
+        TR.openEvent = false;
+
+        IRC.SendMsg("Event vote ended. Option1 - "+ TR.votes["Option1"].Count + " votes Option2 - "+ TR.votes["Option2"].Count+" votes");
+        Debug.Log("EndEvent");
+
+        StartNewEvent();
+    }
+
+	void Start ()
+    {
+        IRC = this.GetComponent<TwitchIRC>();
+        TR = this.GetComponent<TwitchResponses>();
+
+        StartNewEvent();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (timeIsTicking) {
+            time -= Time.deltaTime;
+            if (time <= 0.0f)
+            {
+                EventStop();
+            }
+        }
+	}
+}
---
+++ Assets/TwitchIntegration/TwitchResponses.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwitchResponses : MonoBehaviour {
+
+    public bool openEvent = false;
+    public Dictionary<string, List<string>> votes;
+
+    void Awake() {
+        votes = new Dictionary<string, List<string>>();
+    }
+
+    public void HandleMessage(string user, string msgString)
+    {
+        Debug.Log("MSG:"+msgString);
+
+        if (openEvent)
+        {
+            CheckPublicVote(user, msgString);
+        }
+    }
+
+    public void InitVotes()
+    {
+        votes.Clear();
+        votes.Add("Option1", new List<string> { });
+        votes.Add("Option2", new List<string> { });
+    }
+
+    void CheckPublicVote(string user, string msgString)
+    {
+        Debug.Log("Check");
+        switch (msgString)
+        {
+            case "!vote 1":
+                if (votes["Option1"].IndexOf(user) == -1 && votes["Option2"].IndexOf(user) == -1)
+                {
+                    votes["Option1"].Add(user);
+                } else
+                {
+                    Debug.Log("Already Voted");
+                }
+                
+                break;
+
+            case "!vote 2":
+                if (votes["Option1"].IndexOf(user) == -1 && votes["Option2"].IndexOf(user) == -1)
+                {
+                    votes["Option2"].Add(user);
+                }
+                else
+                {
+                    Debug.Log("Already Voted");
+                }
+
+                break;
+
+            default:
+                return;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
